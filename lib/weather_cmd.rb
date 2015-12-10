5cf4359ae73350016837d0d547f47e427e8fd239
Adam Balali <adam.balali@sap.com>
2015-12-10 15:34:57 +0200

    Added another check of the user input. replaced user input syntax check to regex.

--- lib/weather_cmd.rb
+++ lib/weather_cmd.rb
@@ -2,9 +2,11 @@ require_relative "./weather_cmd/version"
 
 require 'open_weather'
 require 'pry'
+require 'open-uri'
+require 'csv'
 
 module WeatherCmd
-  $options = {
+  OPEN_WEATHER_OPTIONS = {
     units: "metric",
     APPID: ENV['OPEN_WEATHER_API_KEY']
   }
@@ -12,20 +14,34 @@ module WeatherCmd
   def self.parse(user_input)
     @location = ''
 
-    if user_input[0] != 'in'
+    openweather_city_list = 'http://openweathermap.org/help/city_list.txt'
+    content = open(openweather_city_list).read
+    city_list = Hash.new { |key, value| key[value] = Array.new }
+
+    content.each_line do |line|
+      if result = line.match(/\d+\t([a-zA-z ]+)\t[0-9.-]+\t[0-9.-]+\t(\w+)$/)
+        city, state = result.captures
+        city_list[state].push city
+      end
+    end
+
+    if user_input.join(" ") !~ /^in\s/
       abort("Syntax must be: #{$0} in <City>, <State>")
     end
 
-    user_input.each do |item|
-      next if item.equal?("in")
-      @location = @location + ' ' + item
+    if result = user_input.join(" ").match(/^in\s([a-zA-z\- ]+),\s(\w+)/)
+      user_city, user_state = result.captures
+    else
+      abort("Syntax must be: #{$0} in <City>, <State>")
     end
 
     begin
-      current_weather = OpenWeather::Current.city(@location, $options)
+      raise ArgumentError.new("Must be a valid City and State") unless city_list[user_state].include? user_city
+      @location = user_city + ', ' + user_state
+      current_weather = OpenWeather::Current.city(@location, OPEN_WEATHER_OPTIONS)
 
       if current_weather["cod"].equal?("404")
-        raise ArgumentException.new("Must be a valid City and State")
+        raise ArgumentError.new("Must be a valid City and State")
       end
     rescue Exception => msg
       puts msg
